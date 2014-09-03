f1c54a562730a18408c8bf36955e774ec13c15c5
Hongwei Wang <hww712@users.noreply.github.com>
2014-09-03 09:43:38 +0800

    update to 0.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name                    = 'knitter',
-    version                 = '0.2.0',
+    version                 = '0.3.0',
     
     author                  = 'Hongwei Wang',
     author_email            = 'hww712@gmail.com',
