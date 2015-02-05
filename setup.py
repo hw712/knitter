2d53c1fd0e19ae4853775e15c61fc22421b4ea48
Henry.Wang <skymatrix@126.com>
2015-02-05 09:37:29 +0800

    0.3.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name                    = 'knitter',
-    version                 = '0.3.3',
+    version                 = '0.3.4',
     
     author                  = 'Hongwei Wang',
     author_email            = 'hww712@gmail.com',
